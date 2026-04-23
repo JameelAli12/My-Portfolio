0d568a5787fa398c98c64c9b9e9ed7b7607c3cfe
jameelali0101@gmail.com <jameelali0101@gmail.com>
2026-04-23 10:52:07 +0000

    optimize glitch effect

--- transition.js
+++ transition.js
@@ -3,5 +3,5 @@ import {delay} from './utils.js'
 export async function runTransition(wrapper) {
     wrapper.classList.add("glitch")
     await delay(200)
-    wrapper.classList.remove("glitch")
+    wrapper.classList.remove("glitch")     
 }
